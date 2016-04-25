5a2f321dc50d2483712630bf5bc1b4c40a5c1033
Darragh <darragh1992@gmail.com>
2016-04-25 10:03:34 +0100

    intial commit

---
+++ magazines/migrations/0005_auto_20160419_0934.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('magazines', '0004_auto_20160418_1142'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='purchase',
+            name='subscription_end',
+            field=models.DateTimeField(default=datetime.datetime(2016, 4, 19, 9, 34, 32, 576624, tzinfo=utc)),
+        ),
+    ]
